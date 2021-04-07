b2cd080e715f7dd3f814acda27ecf7f63b472e82
CappedCola <57687139+CappedCola@users.noreply.github.com>
2021-04-07 09:47:30 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 playerTurn = true
-
 //create 2d array for board positions
 rows = 3
 columns = 3 
@@ -15,112 +14,129 @@ function setup(){
 }
 
 function draw(){
-    //draw board to start off
-    fill(255)
-    rect(width/2-130, height/2-300, 10, 650, 20)
-    rect(width/2+130, height/2-300, 10, 650, 20)
-    rect(width/2-350, height/2-125, 700, 10, 20)
-    rect(width/2-350, height/2+125, 700, 10, 20)
-    
-    if(playerTurn === false){
-      var madeMove = false
-      for(var i = 0; i < 3; i++){
-          if(madeMove === false){
-            for(var n = 0; n < 3; n++){
-              if(grid[i][n] != 'green' && grid[i][n] != 'red'){
-                grid[i][n] = 'red'
-                madeMove = true
-                break
-              }
-            }
-          }
-        }
-        //computer draws move 
-        fill(255,0,0)
-        if(grid[0][0] === 'red'){
-          circle(width/2-260, height/2-250, 200)
-        }if(grid[0][1] === 'red'){
-          circle(width/2-260, height/2, 200)
-        }if(grid[0][2] === 'red'){
-          circle(width/2-260, height/2+250, 200)         
-        }if(grid[1][0] === 'red'){
-          circle(width/2, height/2-250, 200)
-        }if(grid[1][1] === 'red'){
-          circle(width/2, height/2, 200)
-        }if(grid[1][2] === 'red'){
-          circle(width/2, height/2+250, 200)
-        }if(grid[2][0] === 'red'){
-          circle(width/2+260, height/2-250, 200)
-        }if(grid[2][1] === 'red'){
-          circle(width/2+260, height/2, 200)
-        }if(grid[2][2] === 'red'){
-          circle(width/2+260, height/2+250, 200)
-        }
-        console.log(grid)
-        playerTurn = true
-    }
+  //draw board to start off
+  fill(255)
+  rect(width/2-130, height/2-300, 10, 650, 20)
+  rect(width/2+130, height/2-300, 10, 650, 20)
+  rect(width/2-350, height/2-125, 700, 10, 20)
+  rect(width/2-350, height/2+125, 700, 10, 20)
   
-}   
+  //check if game is over
+  var gridValue = 0
+  for(var i = 0; i < 3; i++){
+    for(var n = 0; n < 3; n++){
+      if(grid[i][n] != undefined){
+        gridValue = gridValue + 1
+      }
+    }
+  }
+  if(playerTurn === false && gridValue < 9){
+    var madeMove = false
+    while(madeMove === false){
+      var randomX = Math.trunc(random(3))
+      var randomY = Math.trunc(random(3))
+      if(grid[randomX][randomY] === undefined){
+        grid[randomX][randomY] = 'red'
+        madeMove = true
+      }
+    }
+    
 
+
+    //computer draws move 
+    fill(255,0,0)
+    if(grid[0][0] === 'red'){
+      circle(width/2-260, height/2-250, 200)
+    }if(grid[0][1] === 'red'){
+      circle(width/2-260, height/2, 200)
+    }if(grid[0][2] === 'red'){
+      circle(width/2-260, height/2+250, 200)         
+    }if(grid[1][0] === 'red'){
+      circle(width/2, height/2-250, 200)
+    }if(grid[1][1] === 'red'){
+      circle(width/2, height/2, 200)
+    }if(grid[1][2] === 'red'){
+      circle(width/2, height/2+250, 200)
+    }if(grid[2][0] === 'red'){
+      circle(width/2+260, height/2-250, 200)
+    }if(grid[2][1] === 'red'){
+      circle(width/2+260, height/2, 200)
+    }if(grid[2][2] === 'red'){
+      circle(width/2+260, height/2+250, 200)
+    }
+    console.log(grid)
+    playerTurn = true
+    
+  }
+}   
 
 function mouseClicked(){
     if (playerTurn === true){
-        fill(0,255,0)
-        if(mouseX > 535 && mouseX < 1295 && mouseY > 70 && mouseY <830){
-            if (mouseX > 1035 && mouseX < 1295){
-                //right side
-                if (mouseY < 320 && mouseY > 70 && grid[2][0] === undefined){
-                    //top 
-                    circle(width/2+260, height/2-250, 200)
-                    grid[2][0] = 'green'
-                }
-                else if (mouseY > 580 && mouseY <830 && grid[2][2] === undefined){
-                    //bottom 
-                    circle(width/2+260, height/2+250, 200)
-                    grid[2][2] = 'green'
-                }
-                else if (mouseY > 320 && mouseY < 580 && grid[2][1] === undefined){
-                    //middle
-                    circle(width/2+260, height/2, 200)
-                    grid[2][1] = 'green'
-                }
-            }
-            if (mouseX < 765 && mouseX > 535){
-                //left side
-                if (mouseY < 320 && mouseY > 70 && grid[0][0] === undefined){
-                    //top 
-                    circle(width/2-260, height/2-250, 200)
-                    grid[0][0] = 'green'
-                }
-                else if (mouseY > 580 && mouseY <830 && grid[0][2] === undefined){
-                    //bottom 
-                    circle(width/2-260, height/2+250, 200)
-                    grid[0][2] = 'green'
-                }
-                else if (mouseY > 320 && mouseY < 580 && grid[0][1] === undefined){
-                    //middle
-                    circle(width/2-260, height/2, 200)                    
-                    grid[0][1] = 'green'
-                }
-            }
-            if (mouseX > 765 && mouseX < 1035){
-                //middle 
-                if (mouseY < 320 && mouseY > 70 && grid[1][0] === undefined){
-                    //top 
-                    circle(width/2, height/2-250, 200)
-                    grid[1][0] = 'green'
-                }
-                else if (mouseY > 580 && mouseY <830 && grid[1][2] === undefined){
-                    //bottom 
-                    circle(width/2, height/2+250, 200)
-                    grid[1][2] = 'green'
-                }
-                else if (mouseY > 320 && mouseY < 580 && grid[1][1] === undefined){
-                    //middle
-                    circle(width/2, height/2, 200)
-                    grid[1][1] = 'green'
-                }
-            }
+      fill(0,255,0)
+      if(mouseX > 535 && mouseX < 1295 && mouseY > 70 && mouseY <830){
+          if (mouseX > 1035 && mouseX < 1295){
+              //right side
+              if (mouseY < 320 && mouseY > 70 && grid[2][0] === undefined){
+                  //top 
+                  circle(width/2+260, height/2-250, 200)
+                  grid[2][0] = 'green'
+                  playerTurn = false
+              }
+              else if (mouseY > 580 && mouseY <830 && grid[2][2] === undefined){
+                  //bottom 
+                  circle(width/2+260, height/2+250, 200)
+                  grid[2][2] = 'green'
+                  playerTurn = false
+              }
+              else if (mouseY > 320 && mouseY < 580 && grid[2][1] === undefined){
+                  //middle
+                  circle(width/2+260, height/2, 200)
+                  grid[2][1] = 'green'
+                  playerTurn = false
+              }
+          }
+          if (mouseX < 765 && mouseX > 535){
+              //left side
+              if (mouseY < 320 && mouseY > 70 && grid[0][0] === undefined){
+                  //top 
+                  circle(width/2-260, height/2-250, 200)
+                  grid[0][0] = 'green'
+                  playerTurn = false
+              }
+              else if (mouseY > 580 && mouseY <830 && grid[0][2] === undefined){
+                  //bottom 
+                  circle(width/2-260, height/2+250, 200)
+                  grid[0][2] = 'green'
+                  playerTurn = false
+              }
+              else if (mouseY > 320 && mouseY < 580 && grid[0][1] === undefined){
+                  //middle
+                  circle(width/2-260, height/2, 200)                    
+                  grid[0][1] = 'green'
+                  playerTurn = false
+              }
+          }
+          if (mouseX > 765 && mouseX < 1035){
+              //middle 
+              if (mouseY < 320 && mouseY > 70 && grid[1][0] === undefined){
+                  //top 
+                  circle(width/2, height/2-250, 200)
+                  grid[1][0] = 'green'
+                  playerTurn = false
+              }
+              else if (mouseY > 580 && mouseY <830 && grid[1][2] === undefined){
+                  //bottom 
+                  circle(width/2, height/2+250, 200)
+                  grid[1][2] = 'green'
+                  playerTurn = false
+              }
+              else if (mouseY > 320 && mouseY < 580 && grid[1][1] === undefined){
+                  //middle
+                  circle(width/2, height/2, 200)
+                  grid[1][1] = 'green'
+                  playerTurn = false
+              }
+          }
            
         }
     
