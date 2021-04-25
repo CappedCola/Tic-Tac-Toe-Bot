33012c802bf532e3f89861d4595fe5b51a2cc398
CappedCola <57687139+CappedCola@users.noreply.github.com>
2021-04-24 21:57:54 -0400

    Add files via upload

---
+++ Tic-Tac-Toe-Bot-main/minimax.js
@@ -0,0 +1,33 @@
+function bestMove(){
+  //ai makes move 
+  let bestScore = -Infinity
+  let move
+  for(let i = 0; i < 3; i++){
+    for(let j = 0; j < 3; j++){
+      //is this spot availble
+      if(grid[i][j] === undefined){
+        grid[i][j] = 'red'
+        let score = minimax(grid, 0, false )
+        grid[i][j] = undefined
+        if(score > bestScore){
+          bestScore = score
+          move = {i,j}
+        }
+      }
+    }
+  }
+  //return return player turn to true && make best move 'red'
+  grid[move.i][move.j] = 'red'
+  playerTurn = true
+  madeMove = true 
+}
+
+let scores = {
+  'X': 1, 
+  'O': -1, 
+  'tie': 0
+}
+
+function minimax(grid, depth, isMaximising){
+  return 1  
+}
